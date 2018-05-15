3e2cd9cc4d6d5bcd94174398ec36f4b6f08f7ac1
Vivek Pagadala <vivr@uw.edu>
2018-05-15 16:27:59 -0700

    time input component

--- assetallocation/app.py
+++ assetallocation/app.py
@@ -1,41 +1,19 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from generate_portfolios import getOutput
+
+from generate_portfolios import get_graph_data
+from frontend import timeperiod_input
+from frontend import riskreturn_graph
 
 app = dash.Dash()
-data = getOutput()
-print(data.head())
 
-x, y, text = data.Return, data.Risk, data.Label
 app.layout = html.Div(children=[
     html.H1(children='Asset allocation'),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [{
-                'x': x,
-                'y': y,
-                'text': text,
-                'textfont': dict(
-                    family='sans serif',
-                    size=18,
-                    color='#1f77b4'
-                ),
-                'type': 'line'
-            }],
-            'layout': {
-                'title': 'Risk-Return Chart',
-                'xaxis': {
-                    'title': 'Risk'
-                },
-                'yaxis': {
-                    'title': 'Return'
-                }
-            }
-        }
-    )
+    riskreturn_graph.get_component(*get_graph_data()),
+    timeperiod_input.get_component()
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- assetallocation/generate_portfolios.py
+++ assetallocation/generate_portfolios.py
@@ -1,44 +1,26 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from datetime import date, timedelta
-
 import functions
 
 
-def getOutput():
-    stock = functions.invest_dataframe('../Data/SP500.csv')
-    bond = functions.invest_dataframe('../Data/BAMLCC0A1AAATRIV.csv')
-
+def get_graph_data(from_year=2014, to_year=2018):
     """Generate portfolios ranging from 10-90% stocks with the remainder in bonds.
     4-year time horizon (2014-18), rebalance every 90 days."""
 
-    a = date(2014, 1, 2)
-    b = date(2018, 1, 3)
+    stock = functions.invest_dataframe('../Data/SP500.csv')
+    bond = functions.invest_dataframe('../Data/BAMLCC0A1AAATRIV.csv')
+
+    a = date(from_year, 1, 2)
+    b = date(to_year, 1, 3)
 
     def gen_allocation(stock, bond, stockshare, start=a, end=b, rebal_time=90):
         assert stockshare >= 0 and stockshare <= 1
         alloc = [(stock, stockshare), (bond, 1 - stockshare)]
         return functions.track_portfolio(alloc, rebal_time, a, b)
 
-    s9b1 = gen_allocation(stock, bond, .9)
-    s8b2 = gen_allocation(stock, bond, .8)
-    s7b3 = gen_allocation(stock, bond, .7)
-    s6b4 = gen_allocation(stock, bond, .6)
-    s5b5 = gen_allocation(stock, bond, .5)
-    s4b6 = gen_allocation(stock, bond, .4)
-    s3b7 = gen_allocation(stock, bond, .3)
-    s2b8 = gen_allocation(stock, bond, .2)
-    s1b9 = gen_allocation(stock, bond, .1)
+    portfolios = [gen_allocation(stock, bond, i / 10) for i in range(1, 10)]
 
     labels = [str(100 - x) + "% stocks " + str(x) +
               "% bonds" for x in range(10, 100, 10)]
 
-    output = functions.label_risk_return(labels=labels,
-                                         portfolios=[s9b1, s8b2, s7b3, s6b4,
-                                                     s5b5, s4b6, s3b7, s2b8, s1b9],
-                                         start=a, end=b)
-    return output
-
-
-getOutput()
+    data = functions.label_risk_return(labels=labels, portfolios=portfolios, start=a, end=b)
+    return data.Return.values, data.Risk.values, data.Label.values
