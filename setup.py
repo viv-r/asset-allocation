a310f31613d636c03b528cfbc426ffd2066f101e
Vivek Pagadala <vivr@uw.edu>
2018-06-05 03:02:48 -0700

    clean up

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+__version__ = '0.0.1'
+
+setup(
+    name='asset_allocation',
+    version=__version__,
+    author='viv-r',
+    packages=['asset_allocation'],
+    include_package_data=True,
+    license='MIT',
+    description='DATA 515 Asset Allocation Project',
+    install_requires=[]
+)
