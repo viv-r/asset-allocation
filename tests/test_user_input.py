303dbffc4bb536cca6a3b4ddcb2518b0fd92dde6
Vamsy Atluri <waatluri@gmail.com>
2018-06-04 17:16:10 -0700

    editing tests folder

--- tests/test_user_input.py
+++ tests/test_user_input.py
@@ -8,40 +8,37 @@ import pandas as pd
 import numpy as np
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-PARENT_DIR = os.path.dirname(CURRENT_DIR)
-# GPARENT_DIR = os.path.dirname(PARENT_DIR)
+PARENT_DIR =  os.path.join(os.path.dirname(CURRENT_DIR),'/backend')
 
 sys.path.insert(0, PARENT_DIR)
 
-from assetallocation import user_input as ui
+from backend import user_input as ui
 
-test_user_input = [{
-    'name':'Portfolio 1',
+test_user_input = [{'name':'Portfolio 1', 
     'input': {
-        'Initial investment': 10000,
-        'Investment classes': {
-            'U.S. large-cap stocks (Wilshire index)': 0.4,
-            'U.S. mid-cap stocks (Wilshire index)': 0.3,
-            'U.S. small-cap stocks (Wilshire index)': 0.3
+    'Initial investment': 10000,
+    'Investment classes': {
+        'U.S. large-cap stocks (Wilshire index)': 0.4,
+        'U.S. mid-cap stocks (Wilshire index)': 0.3,
+        'U.S. small-cap stocks (Wilshire index)': 0.3
         },
-        'Rebalancing frequency (days)': 90,
-        'Start date': pd.Timestamp('2008-01-01 00:00:00'),
-        'End date': pd.Timestamp('2018-01-01 00:00:00')
-        }
-    }, {
-        'name': 'Portfolio 2',
-        'input': {
-            'Initial investment': 10000,
-            'Investment classes': {
-                'U.S. large-cap stocks (S&P 500 index)': 0.5,
-                'U.S. Treasury bonds, 0-1 year (S&P index)': 0.25,
-                'U.S. Treasury bonds, 3-5 year (S&P index)': 0.25
-            },
-            'Rebalancing frequency (days)': 90,
-            'Start date': pd.Timestamp('2010-01-01 00:00:00'),
-            'End date': pd.Timestamp('2018-01-01 00:00:00')
-        }
-    }]
+    'Rebalancing frequency (days)': 90,
+    'Start date': pd.Timestamp('2008-01-01 00:00:00'),
+    'End date': pd.Timestamp('2018-01-01 00:00:00')
+    }},
+    {'name': 'Portfolio 2',
+    'input': {
+    'Initial investment': 10000,
+    'Investment classes': {
+        'U.S. large-cap stocks (S&P 500 index)': 0.5,
+        'U.S. Treasury bonds, 0-1 year (S&P index)': 0.25,
+        'U.S. Treasury bonds, 3-5 year (S&P index)': 0.25
+        },
+    'Rebalancing frequency (days)': 90,
+    'Start date': pd.Timestamp('2010-01-01 00:00:00'),
+    'End date': pd.Timestamp('2018-01-01 00:00:00')
+    }}
+    ]
 
 test_user_param_A = {
     'Measure of return': 'Change in log of portfolio value',
@@ -69,14 +66,20 @@ test_user_param_B = {
 
 portfolio_1 = ui.portfolio_from_input(test_user_input[0]['input'])
 portfolio_2 = ui.portfolio_from_input(test_user_input[1]['input'])
-
+s = timer()
 export_data_A = ui.export_user_portfolios(
     user_portfolio_list=[portfolio['input'] for portfolio in test_user_input],
     user_labels=[portfolio['name'] for portfolio in test_user_input],
     user_parameters=test_user_param_A
     )
+e = timer()
+print("Total time to generate set A", e-s)
+
+s = timer()
 export_data_B = ui.export_user_portfolios(
     user_portfolio_list=[portfolio['input'] for portfolio in test_user_input],
     user_labels=[portfolio['name'] for portfolio in test_user_input],
     user_parameters=test_user_param_B
     )
+e = timer()
+print("Total time to generate set B", e-s)
